c2ddd002212ba0657d8bf7d7fa7fe701ac4e2615
henry van dusen <hgv2102@columbia.edu>
2014-03-20 13:53:05 -0400

    First pages commit

---
+++ README.md
@@ -0,0 +1,10 @@
+candusen
+========
+
+all of my stuff that I want to share with the world
+
+you'll need:
+paper.js
+jquery-2.0.0.min.js
+
+
---
+++ eye/script.js
@@ -0,0 +1,92 @@
+//this is my first site on github ;))
+
+var window = project.view.bounds;
+var center = new Point(400,200);
+//create shapes based on screen size
+var outerShape = new Path.Circle(
+	new Point(0,window.height/4)+center,
+	new Size(window.width*.65,(window.height*.65)));
+	
+var middleShape = new Path.Circle(
+	new Point(window.width/4,window.height/4)+center,
+	new Size(window.width*.40,(window.height*.40)));
+
+var innerShape = new Path.Circle(
+	new Point(window.width/2.8,window.height/3.5)+center,
+	new Size(window.width*.18,window.height*.2));
+
+var frameSpeed = 2;
+var step = 0; //gets incremented to change colors
+var stringOuter ="";
+var stringMiddle ="";
+var stringInner ="";
+//initialize the outer shape's colors. each shape is based on r,g,b of outer shape
+var r=255;
+var g = 0;
+var b = 0;
+
+//Can be one of six phases(3 choose 2 combinations of r,g, and b)
+var phase = 0;
+
+//adjust inner and outer circles
+function onMouseMove(event){
+	innerShape.position.x+= event.delta.x/1.5;
+	outerShape.position.x-= event.delta.x;
+	//console.dir(shape.bounds);
+	center+= event.delta/10;
+}
+
+//shift the current colors about the eye
+function onMouseDown(event){
+	temp = r;
+	r=b;
+	b=g;
+	g=temp;
+	phase+=2
+}
+
+//calling next color actually changes the other shapes too..
+function onFrame(event){
+	
+	if(event.count%frameSpeed==0)
+		{
+		nextColor();
+		outerShape.fillColor = stringOuter;
+		middleShape.fillColor = stringMiddle;
+		innerShape.fillColor = stringInner;
+	}
+}
+
+//gets the next color, updates the color strings for each shape
+function nextColor(){
+	switch(phase%6){
+		case 0:
+		g++;
+		break;
+		case 1:
+		r--;
+		break;
+		case 2:
+		b++;
+		break;
+		case 3:
+		g--;
+		break;
+		case 4:
+		r++;
+		break;
+		case 5:
+		b--;
+	}
+	//colors are flipped
+	stringOuter = "rgb("+r+","+g+","+b+")";
+	stringMiddle = "rgb("+b+","+r+","+g+")";
+	stringInner = "rgb("+g+","+b+","+r+")";
+	step++;
+	//keeps track of which phase we're in
+	if(step == 255)
+	{
+		phase++;
+		step = 0;	
+	}
+}
